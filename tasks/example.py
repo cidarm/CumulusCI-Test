6cf92d830f961845ce24e70fdb2216464d6d88f4
Kit La Touche <kit@transneptune.net>
2018-11-26 09:54:55 -0700

    Add fake install failure

--- tasks/example.py
+++ tasks/example.py
@@ -33,8 +33,18 @@ class StaticSleep(Sleep):
             "description": "Task that this preflight is for",
             "required": True,
         },
+        "status_code": {
+            "description": 'Status code to return, default to "ok".',
+            "required": False,
+        },
+        "msg": {"description": "Message to be included", "required": False},
     })
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.name = self.options.get("task_name")
+
+    def _run_task(self):
+        self.return_values["status_code"] = self.options.get("status_code", "ok")
+        self.return_values["task_name"] = self.options.get("task_name")
+        self.return_values["msg"] = self.options.get("msg", "")
